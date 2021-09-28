57f4c404a8c30ef434cc16bd6fdaa06f8cb64211
Sleeping-Suit <hrcho721@gmail.com>
2021-09-28 14:17:44 +0900

    Crawling Basic Setting

---
+++ src/main/java/com/cos/newscraw/PrjNewscrawApplication.java
@@ -0,0 +1,13 @@
+package com.cos.newscraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjNewscrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjNewscrawApplication.class, args);
+	}
+
+}
